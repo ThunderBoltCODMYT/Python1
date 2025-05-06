b7ec9184d99d5c5093f95a33916095d5b7e51b7d
ThunderBoltCODMYT <aramgade@gmail.com>
2025-05-06 16:46:57 +0530

    Add files via upload

---
+++ b.py
@@ -0,0 +1,3 @@
+print('Welcome to the world of python programming!!')
+print('Durga', 'Pranav', 'Priyank','have started a new course !!',sep='****')
+print('wish them good learning experience ')
---
+++ c.py
@@ -0,0 +1,12 @@
+x=3 
+x1= 10
+x_1 = 20
+_x1= 54
+x,y,z=4,9,1
+a=b=c=20
+name=input('Enter your name : ')
+age=int(input('Enter your age : '))
+percent=float(input('Enter your percentage : '))
+print('hello, ',name)
+print('you are ',age,'years old but still very active and joyful')
+print('you have passed in the exam with ',percent,' %')
